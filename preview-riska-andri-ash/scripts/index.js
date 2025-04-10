6776aa3e99c80275a9cdcdf919e943a65bd85164
akubeta <beta.n.awwalin@gmail.com>
2025-04-10 15:48:31 +0700

    upload music

--- preview-riska-andri-ash/scripts/index.js
+++ preview-riska-andri-ash/scripts/index.js
@@ -11,20 +11,20 @@ tailwind.config = {
         // text50 : "#725B50",
 
         secondary50: "#F9FAFB", // Abu-abu sangat terang
-  secondary100: "#F3F4F6", // Abu-abu terang
-  secondary200: "#E5E7EB", // Abu-abu netral
-  primary50: "#D1D5DB", // Abu-abu lembut
-  primary100: "#9CA3AF", // Abu-abu medium
-  primary200: "#6B7280", // Abu-abu gelap
-  text50: "#4B5563",
+        secondary100: "#F3F4F6", // Abu-abu terang
+        secondary200: "#E5E7EB", // Abu-abu netral
+        primary50: "#D1D5DB", // Abu-abu lembut
+        primary100: "#9CA3AF", // Abu-abu medium
+        primary200: "#6B7280", // Abu-abu gelap
+        text50: "#4B5563",
       },
       fontFamily: {
-        cormorant: ['"Cormorant Garamond"', 'serif'],
-        italiana: ['Italiana', 'serif'],
-        josefin: ['"Josefin Sans"', 'sans-serif'],
-        montserrat: ['Montserrat', 'sans-serif'],
-        noto: ['"Noto Sans"', 'sans-serif'],
-        nunito: ['"Nunito Sans"', 'sans-serif'],
+        cormorant: ['"Cormorant Garamond"', "serif"],
+        italiana: ["Italiana", "serif"],
+        josefin: ['"Josefin Sans"', "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        noto: ['"Noto Sans"', "sans-serif"],
+        nunito: ['"Nunito Sans"', "sans-serif"],
       },
     },
   },
