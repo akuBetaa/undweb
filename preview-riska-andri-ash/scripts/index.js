ab6768b0c21ae4900f5a59b5f10e101d359c26b7
akubeta <beta.n.awwalin@gmail.com>
2025-04-10 11:35:43 +0700

    develop order

---
+++ preview-riska-andri-ash/scripts/index.js
@@ -0,0 +1,31 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        // secondary50: "#FDFDFD",
+        // secondary100 : "#FDFCFC",
+        // secondary200 : "#DBD3CE",
+        // primary50 : "#ECE6E3",
+        // primary100 : "#BBB1A7",
+        // primary200 : "#8E766B",
+        // text50 : "#725B50",
+
+        secondary50: "#F9FAFB", // Abu-abu sangat terang
+  secondary100: "#F3F4F6", // Abu-abu terang
+  secondary200: "#E5E7EB", // Abu-abu netral
+  primary50: "#D1D5DB", // Abu-abu lembut
+  primary100: "#9CA3AF", // Abu-abu medium
+  primary200: "#6B7280", // Abu-abu gelap
+  text50: "#4B5563",
+      },
+      fontFamily: {
+        cormorant: ['"Cormorant Garamond"', 'serif'],
+        italiana: ['Italiana', 'serif'],
+        josefin: ['"Josefin Sans"', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+        noto: ['"Noto Sans"', 'sans-serif'],
+        nunito: ['"Nunito Sans"', 'sans-serif'],
+      },
+    },
+  },
+};
