77b6e88f8ccddd1285355c8031af71d571d55420
akubeta <beta.n.awwalin@gmail.com>
2025-04-11 15:56:05 +0700

    update

---
+++ preview-riska-andri-abu/scripts/index.js
@@ -0,0 +1,40 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        // secondary50: "#FDFDFD",
+        // secondary100 : "#FDFCFC",
+        // secondary200 : "#DBD3CE",
+        // primary50 : "#ECE6E3",
+        // primary100 : "#BBB1A7",
+        // primary200 : "#8E766B",
+        // text50 : "#725B50",
+
+        // varian Ash
+        // secondary50: "#F9FAFB",
+        // secondary100: "#F3F4F6",
+        // secondary200: "#E5E7EB",
+        // primary50: "#D1D5DB",
+        // primary100: "#9CA3AF",
+        // primary200: "#6B7280", 
+        // text50: "#4B5563",
+
+        secondary50: "#D1D5DB", // Abu-abu terang
+        secondary100: "#9CA3AF", // Abu-abu medium (warna utama)
+        secondary200: "#6B7280", // Abu-abu gelap
+        primary50: "#4B5563", // Abu-abu lebih gelap
+        primary100: "#374151", // Abu-abu sangat gelap
+        primary200: "#1F2937", // Abu-abu sangat gelap untuk elemen dengan kontras tinggi
+        text50: "#FFFFFF", 
+      },
+      fontFamily: {
+        cormorant: ['"Cormorant Garamond"', "serif"],
+        italiana: ["Italiana", "serif"],
+        josefin: ['"Josefin Sans"', "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        noto: ['"Noto Sans"', "sans-serif"],
+        nunito: ['"Nunito Sans"', "sans-serif"],
+      },
+    },
+  },
+};
