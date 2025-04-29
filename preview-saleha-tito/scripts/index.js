5c0449b8b144058d7a46b8d4f4974295cfd037cf
akubeta <beta.n.awwalin@gmail.com>
2025-04-29 10:12:51 +0700

    update

---
+++ preview-saleha-tito/scripts/index.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          background50: "rgba(239, 239, 239, 1)",
+          background150: "rgb(248, 248, 248)",
+          background100: "rgb(219, 219, 219)",
+          text50: "#494746",
+          text100: "#FDE047",
+        },
+        fontFamily: {
+            cormorant: ['Cormorant', 'serif'],
+            justanotherhand: ['Just Another Hand', 'cursive'],
+            librefranklin: ['Libre Franklin', 'sans-serif'],
+            nothingyoucoulddo: ['Nothing You Could Do', 'cursive'],
+            sansation: ['Sansation', 'sans-serif'],
+            playfair: ['Playfair', 'serif'],
+        },
+      },
+    },
+  };
